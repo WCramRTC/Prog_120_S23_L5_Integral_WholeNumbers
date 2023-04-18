f7f1f10acc25069ed52123002a743ba2c1dd131f
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-04-18 13:34:45 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@
     {
         static void Main(string[] args)
         {
+            // int, unit
+            // short, ushort
+            // byte, sbyte
+            
+            // parse
+            // Parsing is a method used to tell the computer to check if a string is actually a type of number
+
+
+            // math operators : + , - , * , / , %
+            // operations change depending on the types were working with
+            // string + string : Contatenates
+            // number + number : does math
+
             Console.WriteLine("Hello, World!");
         }
     }

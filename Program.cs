3be7283840bca3aac30e2a99744d6fb6bf8985d3
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-04-17 17:25:04 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog_120_S23_L5_Integral_WholeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Prog_120_S23_L5_Integral_WholeNumbers
+ 
